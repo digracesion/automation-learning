d70a9cbebae7cf88706baacbf93bbe445d27afc9
digracesion <mggicay@gmail.com>
2023-03-02 16:00:35 +0900

    added cucumber tests

---
+++ cucumber/steps/HomePage/when.js
@@ -0,0 +1,8 @@
+import { When } from '@wdio/cucumber-framework';
+When(/^The user enters "(.*)" into the search bar$/, keyword => {
+  $(".gLFyf.gsfi").waitForDisplayed(5000);
+  $(".gLFyf.gsfi").click();
+  $(".gLFyf.gsfi").setValue(keyword);
+  $(".aajZCb .gNO89b").waitForDisplayed(5000);
+  $(".aajZCb .gNO89b").click();
+});
